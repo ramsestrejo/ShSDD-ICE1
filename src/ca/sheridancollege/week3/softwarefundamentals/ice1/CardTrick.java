d5f8ba421ee8e72bdc1c524e3fc150c353111002
Andy <caohoa@sheridancollege.ca>
2019-05-25 21:59:09 -0400

    Assignment 1

--- src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
+++ src/ca/sheridancollege/week3/softwarefundamentals/ice1/CardTrick.java
@@ -1,10 +1,10 @@
+package ca.sheridancollege.week3.softwarefundamentals.ice1;
+import java.util.Scanner;
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package ca.sheridancollege.week3.softwarefundamentals.ice1;
-
 /**
  * A class that fills a magic hand of 7 cards with random Card Objects
  * and then asks the user to pick a card and searches the array of cards
@@ -20,13 +20,30 @@ public class CardTrick {
         for (int i=0; i<magicHand.length; i++)
         {
             Card c = new Card();
-            //c.setValue(insert call to random number generator here)
-            //c.setSuit(Card.SUITS[insert call to random number between 0-3 here])
+            c.setValue((int)(Math.random()*(13-1))+1);
+            c.setSuit(Card.SUITS[(int)(Math.random()*3)]);
         }
-        
-        //insert code to ask the user for Card value and suit, create their card
-        // and search magicHand here
+        Scanner input = new Scanner(System.in);
+        Card user = new Card();
+        System.out.println("Pick a value of your card (1 for Ace to 13 for King)");//insert code to ask the user for Card value and suit, create their card
+        int value = input.nextInt();
+        user.setValue(value);
+        System.out.println("Pick a suit of your card (Heart, Diamond, Spade, Club)");
+        String suit = input.next();
+        user.setSuit(suit);
+        boolean flag = false;
+        for (int i=0; i<magicHand.length; i++)
+        {
+            if ((magicHand[i].getValue()==user.getValue())&&(magicHand[i].getSuit()==user.getSuit())){
+                flag = true;
+            }
+        }
+        if (flag){
+            System.out.println("Your card is in the magic hand");
+        }
+        else {
+            System.out.println("Your card isn't in the magic hand");
+        }// and search magicHand here
         //Then report the result here
     }
-    
 }
